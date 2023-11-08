36d102994043ad4bedfde5348926d24f71f12a24
wolegario40@gmail.com <wolegario49@gmail.com>
2023-11-08 11:10:23 -0300

    W.I.P refatora execução do mc-update Ref.:#1

--- Plugin.php
+++ Plugin.php
@@ -1,13 +1,24 @@
 <?php
+
 namespace CorrecaoRepresentanteLegal;
 
-class Plugin extends \MapasCulturais\Plugin{
+use MapasCulturais\i;
+use MapasCulturais\App;
 
-    public function _init(){
+class Plugin extends \MapasCulturais\Plugin
+{
+
+    public function _init()
+    {
+        $app = App::i();
+
+        $app->_config['mailer.templates']['new_account'] = [
+            'title' => i::__("Bem vindo ao {$app->siteName}"),
+            'template' => 'new-account.html'
+        ];
     }
 
     public function  register()
     {
     }
-
 }
--- mc-updates.php
+++ mc-updates.php
@@ -5,124 +5,105 @@ use MapasCulturais\Entities\User;
 
 return [
     'divide agente individuais de diversas contas' => function () {
+        return false;
         $app = App::i();
-        DB_UPDATE::enqueue('User', 'id IN (SELECT user_id FROM agent WHERE type = 1 GROUP BY user_id HAVING count(*) > 1 LIMIT 30)', function (User $user) use ($app) {
+
+        $review_user = new \MapasCulturais\Entities\User;
+        $review_user->authProvider = 'local';
+        $review_user->authUid = 'revisar-cadastro@mapas.com';
+        $review_user->email = 'revisar-cadastro@mapas.com';
+
+        $app->em->persist($review_user);
+        $app->em->flush();
+
+        DB_UPDATE::enqueue('User', 'id IN (SELECT user_id FROM agent WHERE type = 1 GROUP BY user_id HAVING count(*) > 1 )', function (User $user) use ($app, $review_user) {
             $conn = $app->em->getConnection();
 
-            // Verifica o agente Principal do usuario.
+            $old_agent_default = null;
             foreach ($user->agents as $agent) {
-                if ($agent->id == $user->profile->id) {
-
-                    if ($agent->emailPrivado != $user->email) {
-                        foreach ($user->agents as $_agent) {
-
-                            if ($_agent->emailPrivado == $user->email) {
-                                $app->disableAccessControl();
-                                $user->profile = $agent;
-                                $user->save();
-                                $app->enableAccessControl();
-                                $_agent->user = $user;
-                                $_agent->save();
-                                break;
-                            }
-                        }
-                    }
+                if ($user->profile->id == $agent->id) {
+                    $old_agent_default = $agent;
                     break;
                 }
             }
 
+            $old_update_agents = [];
             foreach ($user->agents as $agent) {
-                if ($agent->id != $user->profile->id) {
-                    $_email = $agent->emailPrivado;
-                    $_name = $agent->name;
-                    $_cpf =  $agent->cpf;
-
-                    if ($_email || $_cpf) {
-
-                        if ($_email) {
-                            if ($_email == $user->email) {
-                                // Adicionar flag revisar-cadastro
-                                $id = $conn->fetchScalar("SELECT nextval('agent_meta_id_seq'::regclass)");
-                                $conn->insert('agent_meta', ['id' => $id, 'object_id' => $agent->id, 'key' => 'revisar-cadastro', 'value' => 'revisar-cadastro']);
-                                continue;
-                            }
-                        } else if ($_cpf) {
-                            $_email = $_cpf . '@mapas';
-                        } else {
-                            // Adicionar flag revisar-cadastro
-                            $id = $conn->fetchScalar("SELECT nextval('agent_meta_id_seq'::regclass)");
-                            $conn->insert('agent_meta', ['id' => $id, 'object_id' => $agent->id, 'key' => 'revisar-cadastro', 'value' => 'revisar-cadastro']);
-                            continue;
-                        }
-
-                        $pass = rand(111111, 999999);
-
-                        $cpf = $_cpf;
-                        $cpf = str_replace("-", "", $cpf); // remove "-"
-                        $cpf = str_replace(".", "", $cpf); // remove "."
-
-                        // generate the token hash
-                        $source = rand(3333, 8888);
-                        $cut = rand(10, 30);
-                        $string = password_hash($source, PASSWORD_DEFAULT);
-                        $token = substr($string, $cut, 20);
-
-                        $app->disableAccessControl();
-                        $_user = new \MapasCulturais\Entities\User;
-                        $_user->authProvider = 'local';
-                        $_user->authUid = filter_var($_email, FILTER_SANITIZE_EMAIL);
-                        $_user->email = filter_var($_email, FILTER_SANITIZE_EMAIL);
-
-                        $app->em->persist($_user);
-                        $app->em->flush();
-
-                        $agent->userId = $_user->id;
-                        $agent->save(true);
-                        $agent->refresh();
-                        
-                        $_user->profile = $agent;
-                        $_user->save(true);
-                        $app->enableAccessControl();
-
-                        $baseUrl = $app->getBaseUrl();
-                        $app->redirect($app->controller('auth')->createUrl(''), 401);
-                        $url =$app->baseUrl . 'autenticacao/?t=' . $token;
-
-                        $mustache = new \Mustache_Engine();
-                        $site_name = $app->siteName;
-                        $filename = $app->view->resolveFilename("views/emails", "new-account.html");
-                        $template = file_get_contents($filename);
-                        $content = $mustache->render(
-                            $template,
-                            array(
-                                "url" => $url,
-                                "siteName" => $site_name,
-                                "user" => $_user->profile->name,
-                                "oldUser" => $user->profile->name,
-                                "urlToValidateAccount" =>  $baseUrl . 'auth/confirma-email?token=' . $token,
-                                "baseUrl" => $baseUrl,
-                                "urlImageToUseInEmails" => $app->view->asset('img/mail-image.png', false),
-                            )
-                        );
-
-                        $app->createAndSendMailMessage([
-                            'from' => $app->config['mailer.from'],
-                            'to' => $_user->email,
-                            'subject' => "Bem-vindo ao " . $site_name,
-                            'body' => $content
-                        ]);
-
-                        $app->disableAccessControl();
-                        $_user->{'localAuthenticationPassword'} = password_hash($pass, PASSWORD_DEFAULT);
-                        $_user->{'tokenVerifyAccount'} = $token;
-                        $_user->{'accountIsActive'} = '0';
-                        $_user->save();
-                        $app->enableAccessControl();
-                    } else {
-                        $id = $conn->fetchScalar("SELECT nextval('agent_meta_id_seq'::regclass)");
-                        $conn->insert('agent_meta', ['id' => $id, 'object_id' => $agent->id, 'key' => 'revisar-cadastro', 'value' => 'revisar-cadastro']);
-                    }
+                if ($user->profile->id != $agent->id && $agent->type->id == 1) {
+                    $old_update_agents[] = $agent;
                 }
+            }
+
+            $email_owner_list = [];
+            foreach ($old_update_agents as $agent) {
+                $email_privado = $agent->emailPrivado;
+                $nome = $agent->name;
+                $cpf =  preg_replace('/[^0-9]/i', '', $agent->cpf);
+
+                if (!$email_privado && !$cpf) {
+
+                    $id = $conn->fetchScalar("SELECT nextval('agent_meta_id_seq'::regclass)");
+                    $conn->insert('agent_meta', ['id' => $id, 'object_id' => $agent->id, 'key' => 'mc-revisar-cadastro', 'value' => 'revisar-cadastro-sem-email-e-sem-cpf']);
+
+                    $id = $conn->fetchScalar("SELECT nextval('agent_meta_id_seq'::regclass)");
+                    $conn->insert('agent_meta', ['id' => $id, 'object_id' => $agent->id, 'key' => 'mc-usuario-cadastro-origem', 'value' => $user->profile->id]);
+
+                    continue;
+                }
+
+                $source = rand(3333, 8888);
+                $cut = rand(10, 30);
+                $string = password_hash($source, PASSWORD_DEFAULT);
+                $token = substr($string, $cut, 20);
+
+                if ($cpf && !$email_privado) {
+                    $email_privado = $cpf . "@mapas";
+                }
+
+                $app->disableAccessControl();
+
+                $new_user = new \MapasCulturais\Entities\User;
+                $new_user->authProvider = 'local';
+                $new_user->authUid = $email_privado;
+                $new_user->email = $email_privado;
+
+                $app->em->persist($new_user);
+                $app->em->flush();
+
+                $agent->userId = $new_user->id;
+                $agent->save(true);
+                $agent->refresh();
+
+                $new_user->profile = $agent;
+                $new_user->save(true);
+                $email_owner_list[] = $agent->name;
+
+                $baseUrl = $app->getBaseUrl();
+                $url = $app->baseUrl . 'autenticacao/?t=' . $token;
+                $site_name = $app->siteName;
+
+                $dataValue = [
+                    "siteName" => $site_name,
+                    "user" => $new_user->profile->name,
+                    "oldUser" => $user->profile->name,
+                ];
+
+                $message = $app->renderMailerTemplate('new_account', $dataValue);
+
+                $app->createAndSendMailMessage([
+                    'from' => $app->config['mailer.from'],
+                    'to' => $new_user->email,
+                    'subject' => $message['title'],
+                    'body' => $message['body']
+                ]);
+
+                $pass = rand(111111, 999999);
+                $new_user->{'localAuthenticationPassword'} = password_hash($pass, PASSWORD_DEFAULT);
+                $new_user->{'tokenVerifyAccount'} = $token;
+                $new_user->{'accountIsActive'} = '0';
+                $new_user->save();
+
+                $app->enableAccessControl();
             }
         });
 
