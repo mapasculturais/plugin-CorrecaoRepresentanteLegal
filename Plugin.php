4c89aae72d203f8c5a6a87ca32f8b375a5cca085
erleibiazzio <erleibiazzio@gmail.com>
2023-11-10 19:14:34 -0300

    Ajusta a configuração do plugin para rodar dentro do contrutor

--- Plugin.php
+++ Plugin.php
@@ -7,15 +7,29 @@ use MapasCulturais\App;
 
 class Plugin extends \MapasCulturais\Plugin
 {
+    function __construct($config = [])
+    {
+        $app = App::i();
+        $email_messages = [
+            'new_account' => [
+                'title' => i::__("Bem vindo ao {$app->siteName}"),
+                'template' => 'new-account.html'
+            ],
+            'old_account' => [
+                'title' => i::__("Alterações de agentes para atenter a LGPD no {$app->siteName}"),
+                'template' => 'old-account.html'
+            ],
+
+        ];
+
+        $app->_config['mailer.templates'] += $email_messages;
+
+        parent::__construct($config);
+    }
 
     public function _init()
     {
         $app = App::i();
-
-        $app->_config['mailer.templates']['new_account'] = [
-            'title' => i::__("Bem vindo ao {$app->siteName}"),
-            'template' => 'new-account.html'
-        ];
     }
 
     public function  register()
